55ce7fe488d5b7f346df9cd61db606f2ce6826f5
aluxey <aurelien.luxeyy@gmail.com>
2025-02-01 18:06:15 +0100

    Add gitignore and config file

---
+++ config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+# TWITCH_CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
+# TWITCH_CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
+# GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+# CALENDAR_API_KEY = os.getenv("CALENDAR_API_KEY")
+# DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///data/database.db")
+# LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
